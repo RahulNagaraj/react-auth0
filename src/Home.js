506223e2d8cbc0797b3db1cd5e9fed3e5fef3a0b
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-08 23:22:11 +0530

    auth login

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,11 @@ import React, { Component } from "react";
 
 export default class Home extends Component {
   render() {
-    return <h1>Home</h1>;
+    return (
+      <>
+        <h1>Home</h1>
+        <button onClick={this.props.auth.login}>Log In</button>
+      </>
+    );
   }
 }
