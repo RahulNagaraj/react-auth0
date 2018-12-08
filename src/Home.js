e66d99d2a1c3f8a70a55ab90d7b1082be73a1b62
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-09 00:57:04 +0530

    get user profile info

--- src/Home.js
+++ src/Home.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 
 export default class Home extends Component {
   render() {
+    const { isAuthenticated, login } = this.props.auth;
     return (
       <>
         <h1>Home</h1>
-        {this.props.auth.isAuthenticated() ? (
+        {isAuthenticated() ? (
           <Link to="/profile"> View Profile</Link>
         ) : (
-          <button onClick={this.props.auth.login}>Log In</button>
+          <button onClick={login}>Log In</button>
         )}
       </>
     );
