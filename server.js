ed5767b2b39e63db929c6bf6dda2bfa5886d9898
Rahul N <rahul.nagarajmv@gmail.com>
2018-12-09 12:07:37 +0530

    create a private api

--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require("express");
+const jwt = require("express-jwt");
+const jwks = require("jwks-rsa");
 require("dotenv").config();
+
+const jwtCheck = jwt({
+  secret: jwks.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${
+      process.env.REACT_APP_AUTH0_DOMAIN
+    }/.well-known/jwks.json`
+  }),
+  audience: `${process.env.REACT_APP_AUTH0_AUDIENCE}`,
+  issuer: `${process.env.REACT_APP_AUTH0_DOMAIN}`,
+  algorithms: ["RS256"]
+});
 
 const app = express();
 
 app.get("/public", (req, res) => {
   res.json({
     message: "Hello from public api"
+  });
+});
+
+app.get("/private", jwtCheck, (req, res) => {
+  res.json({
+    message: "Hello from private api"
   });
 });
 
